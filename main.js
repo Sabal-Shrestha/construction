c9360d949667f9157dd6005ddcb9616de1cee6f0
Sabal Shrestha <sabalshrestha427@gmail.com>
2023-04-26 22:15:20 +0545

    special services paralalx

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
-// const parallax_el = document.querySelectorAll(".parallax");
-// const main = document.querySelector(".architecture>.hero");
-// let xValue = 0,
-//   yValue = 0;
+const parallaxContainers = document.querySelectorAll(".parallax-container");
 
-// window.addEventListener("mousemove", (e) => {
-//   xValue = e.clientX - window.innerHeight / 2;
-//   yValue = e.clientY - window.innerHeight / 2;
+parallaxContainers.forEach((parallaxContainer) => {
+  const parallax_mel = parallaxContainer.querySelectorAll(".parallax-mouse");
+  let mxValue = 0,
+    myValue = 0;
 
-//   parallax_el.forEach((el) => {
-//     let speedx = el.dataset.speedx;
-//     let speedy = el.dataset.speedy;
-//     el.style.transform = `translateX(calc(-50% + ${
-//       -xValue * speedx
-//     }px)) translateY(calc(-50% + ${yValue * speedy}px))`;
-//   });
-// });
+  parallaxContainer.addEventListener("mousemove", (e) => {
+    mxValue = e.clientX - parallaxContainer.offsetLeft - window.innerHeight / 2;
+    myValue = e.clientY - parallaxContainer.offsetTop - window.innerHeight / 2;
 
-const main = document.querySelector(".architecture");
-const parallax_el = document.querySelectorAll(".parallax");
-let yValue = 0;
+    parallax_mel.forEach((el) => {
+      let speedx = el.dataset.speedx;
+      let speedy = el.dataset.speedy;
+      el.style.transform = `
+        scale(1.4)
+        translateX(calc(-15% - ${mxValue * speedx}px))
+        translateY(calc(-50% - ${myValue * speedy}px))
+        `;
+    });
+  });
+
+  parallaxContainer.addEventListener("mouseout", () => {
+    parallax_mel.forEach((el) => {
+      el.style.transform = "";
+    });
+  });
+});
+
+const parallax_sel = document.querySelectorAll(".parallax-scroll");
+let syValue = 0;
 
 window.addEventListener("scroll", (e) => {
   //   xValue = window.scrollX;
-  yValue = window.scrollY;
+  syValue = window.scrollY;
 
-  parallax_el.forEach((el) => {
+  parallax_sel.forEach((el) => {
     let speedx = el.dataset.speedx;
     let speedy = el.dataset.speedy;
     el.style.transform = `translateX(calc(-50% + ${
-      -yValue * speedx
-    }px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+      -syValue * speedx
+    }px)) translateY(calc(-50% + ${syValue * speedy}px))`;
   });
 });
